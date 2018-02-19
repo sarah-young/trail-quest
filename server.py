0d264cbcdc9057472c45f39874faa4a502af01ae
Sarah Dicker <sarah@sarahs-mbp.lan>
2018-02-19 13:46:51 -0800

    Refactored code for homepage. Git commiting while everything is still working. Map is showing up as a grey square.

--- server.py
+++ server.py
@@ -39,7 +39,7 @@ def display_trail_form():
 # 	print "COORDINATES: ", coordinates
 
 # 	session["coordinates"] = coordinates
-# 	radius_to_meters = int(radius) * 1609.34 
+# 	radius_to_meters = int(radius) * 1609.34
 # 	session["radius"] = radius_to_meters
 # 	# convert radius to meters for use on google map!
 
@@ -77,9 +77,7 @@ def asynchronous_info_load():
 	# loop through iterable to get de
 
 	print "REQUEST FORM DATA: ", request.form.get("data")
-
 	city = request.form.get("city")
-
 	print "CITY: ", city
 	state = request.form.get("state")
 	print "STATE: ", state
@@ -91,47 +89,47 @@ def asynchronous_info_load():
 	print "DIFFICULTY SELECTED: ", trail_difficulty
 
 	coordinates = functions.find_lat_lng(city,state)
+	# ***Google Map API gets called here!***
 	print "COORDINATES: ", coordinates
 
-	session["coordinates"] = coordinates
-	radius_to_meters = int(radius) * 1609.34 
-	session["radius"] = radius_to_meters
-	# convert radius to meters for use on google map!
+	radius_to_meters = int(radius) * 1609.34
 
 	if coordinates == None:
 		flash("Hmm. No trails were found. Try another location?")
 		return render_template('/homepage.html')
 
+
 	trails = functions.find_trails(coordinates, radius)
-	# Hiking API gets called here!
+	# ***Hiking API gets called here!***
 
 	if len(trails) == 0:
 		flash("Hmm. No trails were found. Try another location?")
 		return render_template('/homepage.html')
 
 	trails_to_db = functions.add_trails_to_db(trails)
+	# Adds all trails from hiking project API to database
 	trails = functions.filter_trek_length(trails, trek_length)
+	# filters trail returned by API by user trail length preference
 	print "TRAILS AFTER LENGTH FILTER: ", trails
 
 	trails = functions.filter_trek_difficulty(trails, trail_difficulty)
+	# filters trails returned by API for user trail difficulty preference
+
 	print "TRAILS AFTER DIFFICULTY FILTER: ", trails
 
-	session['selected_trails'] = functions.select_three_trails(trails)
-	print "SESSION: selected_trails: ", session['selected_trails']
+ 	selected_trails = functions.select_three_trails(trails)
+	print "SELECTED TRAILS OBJECT TYPE: ", type(selected_trails)
 
-	selected_trails = session['selected_trails']
-
-	session['location'] = (city, state,)
-
-	google_maps_api_key = secrets.SATELLITE_MAP_GM_API_KEY
-
-	coordinates = session['coordinates'] # lat/long from Google Maps API call 
 	lat, lng = coordinates
 	lat = float(lat)
 	lng = float(lng)
-	radius_in_meters = session['radius']
 
-	
+	selected_trails[0]["city_lat"] = lat
+	selected_trails[0]["city_long"] = lng
+	selected_trails[0]["radius_in_meters"] = radius_to_meters
+	session['radius'] = radius_to_meters
+	# Keeping for now to see if this works with other routes... Not sure if this is needed.
+
 	return jsonify(selected_trails)
 
 @app.route('/trails')
@@ -145,11 +143,12 @@ def display_selected_trails():
 
 	google_maps_api_key = secrets.SATELLITE_MAP_GM_API_KEY
 
-	coordinates = session['coordinates'] # lat / long from Google Maps API call 
+	coordinates = session['coordinates'] # lat / long from Google Maps API call
 	lat, lng = coordinates
 	city_latitude = float(lat)
 	city_longitude = float(lng)
 	radius_in_meters = session['radius']
+	# this isn't needed anymore? Or is it??? Maybe this is easier than sending stuff back and forth??? Keep this for now..
 
 	return render_template('/trails.html', selected_trails=selected_trails, city=city, state=state, api_key=google_maps_api_key, city_latitude=lat, city_longitude=lng, radius=radius_in_meters)
 	# passing selected_trails list, city, state, api key for google maps, and lat/long for map
@@ -157,6 +156,8 @@ def display_selected_trails():
 @app.route('/trek')
 def show_trail_location():
 	"""Displays trail location on map and more information about the trail."""
+
+	# TODO: Refactor so this works with the one-page app format
 
 	chosen_trail_id = request.args.get("trail_id")
 	trail_conditions = functions.get_trail_conditions(chosen_trail_id)
@@ -176,8 +177,3 @@ if __name__ == "__main__":
     DebugToolbarExtension(app)
     connect_to_db(app)
     app.run(host='0.0.0.0')
-
-
-
-
-
